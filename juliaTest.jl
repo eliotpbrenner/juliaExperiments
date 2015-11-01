0e1fe58d99f459ca75e1e368438057acb36f7a9e
eliot <eliotpbrenner@gmail.com>
2015-10-31 20:54:16 -0400

    mutual information

--- juliaTest.jl
+++ juliaTest.jl
@@ -7,7 +7,7 @@ typeof(dataArray)
 typeof(nodes)
 typeof(nval)
 
-
+tol=1e-20
 workspace() #use this to clear definitions
 type model
   nodes::Int64
@@ -32,18 +32,49 @@ function verify(aJointDistributionWithMarginals::jointDistWithMarginals)
   sumB=sum(aJointDistributionWithMarginals.pdist,2)
   println(sumB);
   println(aJointDistributionWithMarginals.p_A)
-  assert(maximum(transpose(sumA)-aJointDistributionWithMarginals.p_A) < 1e-20)
-  assert(maximum(sumB-aJointDistributionWithMarginals.p_B) < 1e-20)
-  assert(sum(sumA)-1.0 < 1e-20)
+  @test_approx_eq(transpose(sumA),aJointDistributionWithMarginals.p_A)
+  @test_approx_eq(sumB,aJointDistributionWithMarginals.p_B)
+  @test_approx_eq(sum(sumA),1.0)
+  return true
 end
 
 using Base.Test
 @test 1 == 1
 @test 1==0
 goodDist=jointDistWithMarginals([[0.25 0.25], [ 0.25 0.25]], [0.5, 0.5], [0.5, 0.5])
-verify(goodDist)
+@test verify(goodDist) == true
+badDist = jointDistWithMarginals([[0.25 0.25], [ 0.25 0.25]], [0.6, 0.4], [0.5, 0.5])
+@test_throws ErrorException verify(badDist)
+goodDist.pdist[1,2]
+
+function MI_term(i::Int, j::Int, dist::jointDistWithMarginals)
+  return -dist.pdist[i,j]*log(dist.pdist[i,j]/(dist.p_A[i]*dist.p_B[j])  )
+end
+
+function MI(dist::jointDistWithMarginals)
+  (a,b)=size(dist.pdist)
+  return sum([(MI_term(i,j,dist)) for i=1:a, j=1:b])
+end
+
+MI_term(1,1,goodDist)
+@test_approx_eq(MI(goodDist),0)
+
+
+verify(badDist)
 
 goodDist.pdist
 sum(goodDist.pdist, 1)
 goodDist.p_A
 
+function conditionalDist(m::model,i::int,j::int,SToVal::Dict)
+  """
+  TODO: Test that i,j are distinct and not in conditioning set
+  that S is a dict of integers and that both are valid for the model
+  """
+  selectedPoints = reduce(&,[dataArray[:,condVar] .==ad[condVar] for condVar in keys(SToVal)])
+
+
+"""
+reduce(&,Array[d[:,3] .==1, d[:,5] .==1])
+reduce(&,[d[:,k] .==ad[k] for k in keys(ad)])
+reduce(&,[dataArray[:,condVar] .==ad[condVar] for condVar in keys(SToVal)])"""
